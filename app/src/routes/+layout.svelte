36fb5bc4cdb2444d584c6208bd323f757c1cc124
Suprakit Sujariyakul <suprakit.s@opn.ooo>
2025-09-08 07:21:22 +0700

    i18n

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { page } from '$app/state';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
@@ -9,3 +11,15 @@
 </svelte:head>
 
 {@render children?.()}
+
+<div class="hidden">
+	{#each locales as locale}
+		<a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+	{/each}
+</div>
+
+<style>
+	.hidden {
+		display: none;
+	}
+</style>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import { ScreeningPage } from '@core/auth'
+	import { m } from '$lib/paraglide/messages'
 </script>
 
 <ScreeningPage 
-	title="Job Application Portal"
+	title={m.common_hello_world({ name: 'John' })}
 	subtitle="Join our team! Please complete this screening form to start your application process."
 />
